303c26756f1a87701cd4f90a63b4ae635c2f0eeb
macihasa <macihasa@gmail.com>
2023-12-04 11:06:49 +0100

    Finished both parts of day 2!

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"C"
 	"log"
 	"os"
-	"path/filepath"
 	"strconv"
 	"strings"
 )
+import "fmt"
 
 func main() {
-	day1()
+	// day1()
+	day2part2()
 
 }
 
@@ -35,7 +36,6 @@ func day1() {
 
 		var firstnum int
 		var lastnum int
-		fmt.Printf("line: %v\n", line)
 
 		for i, char := range line {
 			// Numbers represented as 1 2 3...
@@ -66,17 +66,131 @@ func day1() {
 		}
 		sum += firstnum * 10
 		sum += lastnum
-		fmt.Printf("firstnum: %v\n", firstnum)
-		fmt.Printf("lastnum: %v\n", lastnum)
+		// fmt.Printf("line: %v\n", line)
+		// fmt.Printf("firstnum: %v\n", firstnum)
+		// fmt.Printf("lastnum: %v\n", lastnum)
 	}
 
-	fmt.Println(sum)
+	// fmt.Println(sum)
+}
+
+func day2() {
+
+	const MAX_NUM_RED = 12
+	const MAX_NUM_GREEN = 13
+	const MAX_NUM_BLUE = 14
+
+	games := readTxtFile("day2input.txt")
+
+	var idsum int
+
+	for _, game := range games {
+		fmt.Printf("game: %v\n", game)
+		gameparts := strings.Split(game, ":")
+		id, err := strconv.Atoi(strings.Trim(gameparts[0], "Game "))
+		if err != nil {
+			log.Println("unable to convert game id to integer:", err)
+		}
+		fmt.Printf("id: %v\n", id)
+
+		gameIsValid := true
+		sets := strings.Split(gameparts[1], "; ")
+		for _, set := range sets {
+
+			if !gameIsValid {
+				break
+			}
+
+			// fmt.Printf("set: %v %v\n", i, set)
+			pairs := strings.Split(set, ", ")
+
+			for _, pair := range pairs {
+
+				// fmt.Printf("pair: %v\n", pair)
+
+				pair = strings.TrimSpace(pair)
+				partsOfPair := strings.Split(pair, " ")
+				num, err := strconv.Atoi(partsOfPair[0])
+				if err != nil {
+					fmt.Println("unable to convert cube count to int", err, partsOfPair)
+				}
+
+				if partsOfPair[1] == "red" && num > MAX_NUM_RED {
+					gameIsValid = false
+					fmt.Printf("Game invalid, %v  %v\n", partsOfPair[1], num)
+					break
+				}
+				if partsOfPair[1] == "green" && num > MAX_NUM_GREEN {
+					gameIsValid = false
+					fmt.Printf("Game invalid, %v  %v\n", partsOfPair[1], num)
+					break
+				}
+				if partsOfPair[1] == "blue" && num > MAX_NUM_BLUE {
+					gameIsValid = false
+					fmt.Printf("Game invalid, %v  %v\n", partsOfPair[1], num)
+					break
+				}
+
+			}
+		}
+
+		if gameIsValid {
+			idsum += id
+		}
+
+	}
+	fmt.Printf("idsum: %v\n", idsum)
+}
+
+func day2part2() {
+
+	games := readTxtFile("day2input.txt")
+
+	var powerSum int
+
+	for _, game := range games {
+		gameparts := strings.Split(game, ":")
+		var MAX_RED int
+		var MAX_GREEN int
+		var MAX_BLUE int
+
+		sets := strings.Split(gameparts[1], "; ")
+		for _, set := range sets {
+
+			// fmt.Printf("set: %v %v\n", i, set)
+			pairs := strings.Split(set, ", ")
+
+			for _, pair := range pairs {
+
+				// fmt.Printf("pair: %v\n", pair)
+
+				pair = strings.TrimSpace(pair)
+				partsOfPair := strings.Split(pair, " ")
+				num, err := strconv.Atoi(partsOfPair[0])
+				if err != nil {
+					fmt.Println("unable to convert cube count to int", err, partsOfPair)
+				}
+
+				if partsOfPair[1] == "red" && num > MAX_RED {
+					MAX_RED = num
+				}
+				if partsOfPair[1] == "green" && num > MAX_GREEN {
+					MAX_GREEN = num
+				}
+				if partsOfPair[1] == "blue" && num > MAX_BLUE {
+					MAX_BLUE = num
+				}
+
+			}
+		}
+		fmt.Printf("game: %v\npowers: red %v, green %v, blue %v. Multiplied %v\n\n", game, MAX_RED, MAX_GREEN, MAX_BLUE, MAX_RED*MAX_GREEN*MAX_BLUE)
+
+		powerSum += MAX_RED * MAX_GREEN * MAX_BLUE
+	}
+	fmt.Printf("powerSum: %v\n", powerSum)
 }
 
 func readTxtFile(filename string) []string {
-	if filepath.Ext(filename) != "txt" {
-		fmt.Println("not a txt file!")
-	}
 	text, err := os.ReadFile(filename)
 	if err != nil {
 		log.Fatal("unable to read file", err)
